d25305139b110fd1a7db522b4994dda0bc065be6
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-03-14 20:51:30 +0300

    Index içeriği düzenlendi.

--- Starcups/HeadOffice/Pages/Index.cshtml.cs
+++ Starcups/HeadOffice/Pages/Index.cshtml.cs
@@ -1,25 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
 
 namespace HeadOffice.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        public OrderData[] Orders { get; set; }
 
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
         }
 
-        public void OnGet()
+        public async Task OnGet([FromServices] OrderClient client)
         {
-
+            Orders = await client.GetOrdersAsync();
         }
     }
 }
